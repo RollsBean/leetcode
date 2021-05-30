e0d7f6a2995ff0845eae3f8ff9c3c8d728e175fe
Kevin Fan <rollsbean@gmail.com>
2021-05-30 21:07:54 +0800

    [Leetcode-146][Add][LRU cache]

---
+++ src/a_146_lruCache/LRUCache.java
@@ -0,0 +1,32 @@
+package a_146_lruCache;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author 景行
+ * @date 2021/05/30
+ **/
+public class LRUCache {
+
+    private static LinkedHashMap<Integer, Integer> map;
+
+
+
+    public LRUCache(int capacity) {
+        map = new LinkedHashMap(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return this.size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+}
