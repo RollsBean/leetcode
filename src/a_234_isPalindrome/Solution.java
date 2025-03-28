fe98e9cfb78f5e52be1a790131d7af7aa8191a70
Kevin Fan <rollsbean@gmail.com>
2025-03-28 17:29:55 +0800

    [Leetcode-234][Add][Is Palindrome linkedList]

---
+++ src/a_234_isPalindrome/Solution.java
@@ -0,0 +1,52 @@
+package a_234_isPalindrome;
+
+import b_listBean.ListNode;
+
+import java.util.ArrayDeque;
+
+/**
+ * 是否是回文链表
+ */
+public class Solution {
+
+    /**
+     *
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome(ListNode head) {
+        ArrayDeque<ListNode> stack = new ArrayDeque<>();
+        ListNode pivot = head;
+        while (pivot != null) {
+            stack.push(pivot);
+            pivot = pivot.next;
+        }
+        while (head != null) {
+            ListNode pop = stack.pop();
+            if (head.val != pop.val) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+
+    private void recursive(ListNode head, ArrayDeque<ListNode> stack) {
+        if (head == null) {
+            return;
+        }
+        recursive(head.next, stack);
+        System.out.println(head.val);
+        stack.push(head);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+
+        boolean palindrome = solution.isPalindrome(head);
+        System.out.println(palindrome);
+    }
+}
