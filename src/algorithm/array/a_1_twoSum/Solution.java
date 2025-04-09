df9969c5e555030e93694ebad4fe4602d703e1cf
Kevin Fan <rollsbean@gmail.com>
2025-04-09 10:18:33 +0800

    [Leetcode-1][Add][Two sums]

---
+++ src/algorithm/array/a_1_twoSum/Solution.java
@@ -0,0 +1,25 @@
+package algorithm.array.a_1_twoSum;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * 1. 两数之和，返回相加等于 target 的数组下标（从小到大顺序排列）
+ */
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>(nums.length + 1);
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int num = nums[i];
+            int j = target - num;
+            if (map.containsKey(j)) {
+                return new int[]{i, map.get(j)};
+            } else {
+                map.put(num, i);
+            }
+        }
+        return new int[0];
+    }
+
+}
