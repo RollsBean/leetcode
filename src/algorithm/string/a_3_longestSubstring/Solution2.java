36459c0444e2872cc3a10cfc3f24b25763871db2
Kevin Fan <rollsbean@gmail.com>
2025-04-09 11:21:35 +0800

    [Leetcode-3][Update][Length of longest sub String-sliding window]

---
+++ src/algorithm/string/a_3_longestSubstring/Solution2.java
@@ -0,0 +1,38 @@
+package algorithm.string.a_3_longestSubstring;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 3. 无重复的最长字串
+ */
+public class Solution2 {
+
+    public int lengthOfLongestSubstring(String s) {
+        char[] charArray = s.toCharArray();
+        if (charArray.length == 0) {
+            return 0;
+        }
+
+        int maxLength = 0;
+        int j = 0;
+        Set<Character> sets = new HashSet<>();
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            while (sets.contains(c)) {
+                sets.remove(charArray[j]);
+                j++;
+            }
+            sets.add(c);
+            maxLength = Math.max(maxLength, i - j + 1);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        Solution2 solution2 = new Solution2();
+        int i = solution2.lengthOfLongestSubstring(s);
+        System.out.println(i);
+    }
+}
