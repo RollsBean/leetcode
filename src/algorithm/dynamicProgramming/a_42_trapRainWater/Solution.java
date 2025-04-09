48dbd3f251f023ba99331f97f6d7b225cd476100
Kevin Fan <rollsbean@gmail.com>
2025-04-09 15:47:36 +0800

    [Leetcode-42][Add][Trap rainwater - dp: need two arrays record the left and right max value]

--- src/algorithm/dynamicProgramming/a_42_trapRainWater/Solution.java
+++ src/algorithm/dynamicProgramming/a_42_trapRainWater/Solution.java
@@ -8,40 +8,23 @@ class Solution {
             return 0;
         }
         int[] maxL = new int[length];
+        maxL[0] = 0;
         int[] maxR = new int[length];
-        for (int i = 0; i < length; i++) {
-            int maxLV = 0;
-            int maxRV = 0;
-            for (int j = 0; j < i; j++) {
-                // 左边最大
-                if (height[j] > maxLV) maxLV = height[j];
-            }
-            maxL[i] = maxLV;
-
-            for (int j = i; j < length; j++) {
-                // 右边最大
-                if (height[j] > maxRV) maxRV = height[j];
-            }
-            maxR[i] = maxRV;
+        maxR[length-1] = 0;
+        for (int i = 1; i < length; i++) {
+            // 定义 dp[i] 是第 i+1 个元素左边的最大值，公式 dp[i] = max( dp[i-1], height[i-1])
+            maxL[i] = Math.max(maxL[i-1], height[i-1]);
+        }
+        for (int j = length - 2; j > 0; j--) {
+            // 定义 dp[i] 是第 i+1 个元素右边的最大值，公式 dp[i] = max( dp[i+1], height[i+1])
+            maxR[j] = Math.max(maxR[j+1], height[j+1]);
         }
         int result = 0;
         for (int i = 0; i < length; i++) {
             int minSide = Math.min(maxL[i], maxR[i]);
-            result +=  Math.max(minSide - height[i], 0);
+            result += Math.max(minSide - height[i], 0);
         }
         return result;
-    }
-
-    private int getMax(int[] height) {
-        int i = 1;
-        int max = height[0];
-        while (i < height.length) {
-            if (height[i] > max) {
-                max = height[i];
-            }
-            i++;
-        }
-        return max;
     }
 
     public static void main(String[] args) {
