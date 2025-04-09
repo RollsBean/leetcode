2ea80c9d9bc47f5e33f53daa6afa158979679741
Kevin Fan <rollsbean@gmail.com>
2025-04-09 18:24:50 +0800

    [Leetcode][Update][Move TreeNode package]

--- src/algorithm/binaryTree/a_226_invertTree/Solution.java
+++ src/algorithm/binaryTree/a_226_invertTree/Solution.java
@@ -1,5 +1,7 @@
 package algorithm.binaryTree.a_226_invertTree;
 
+import algorithm.binaryTree.TreeNode;
+
 /**
  * @author 景行
  * @date 2021/05/30
@@ -23,24 +25,5 @@ public class Solution {
         root.left = invertTree(root.left);
         root.right = invertTree(root.right);
         return root;
-    }
-
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
     }
 }
