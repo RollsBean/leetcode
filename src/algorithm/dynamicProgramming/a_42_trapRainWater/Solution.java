1cdcd320a209a890e3157ee71d56b6e7379939e2
Kevin Fan <rollsbean@gmail.com>
2025-04-09 16:43:12 +0800

    [Leetcode-42][Update][Trap rainwater - two pointers]

--- src/algorithm/dynamicProgramming/a_42_trapRainWater/Solution.java
+++ src/algorithm/dynamicProgramming/a_42_trapRainWater/Solution.java
@@ -1,8 +1,16 @@
 package algorithm.dynamicProgramming.a_42_trapRainWater;
 
+/**
+ * 42. 接雨水
+ */
 class Solution {
 
-    protected int trap(int[] height) {
+    /**
+     * <strong>动态规划</strong>求每个元素左右两侧的最大值，需要额外两个数组分别保存，左边最大值和右边的最大值
+     * @param height
+     * @return
+     */
+    public int trap(int[] height) {
         int length = height.length;
         if (length == 1) {
             return 0;
@@ -27,10 +35,46 @@ class Solution {
         return result;
     }
 
+    /**
+     * dp 改进
+     * <p/> 左右指针移动，两个指针分别指向第二个元素和倒数第二个元素，短板效应所以短边确定高度，确定后，当前元素的接水量就确定了，短边在左移
+     * 动左边指针，短边在右移动右边指针
+     * <br/>左右两个指针即是左右最大值的起始点，又是计算元素大小的点，比如左指针，左指针即用 leftMax 标识左边最大值，又标识当前计算接水的节点在
+     * 左指针的位置
+     * @param height
+     * @return
+     */
+    public int trap2(int[] height) {
+        int length = height.length;
+        if (length == 1) {
+            return 0;
+        }
+        int left = 1;
+        int right = length - 2;
+        int leftMax = height[0];
+        int rightMax = height[length - 1];
+        int result = 0;
+        while (left <= right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (leftMax < rightMax) {
+//                result += Math.max(leftMax - height[left], 0);
+                result += leftMax - height[left];
+                left++;
+            } else {
+//                result += Math.max(rightMax - height[right], 0);
+                result += rightMax - height[right];
+                right--;
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] height = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
-        int trap = solution.trap(height);
+        int trap = solution.trap2(height);
         System.out.println(trap);
     }
 }
