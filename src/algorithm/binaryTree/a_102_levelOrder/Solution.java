723f8c3d2d95fe283566172a30cbba8cd9b92f79
Kevin Fan <rollsbean@gmail.com>
2025-04-14 10:44:41 +0800

    [Leetcode-102][Update][Binary tree level order - using Deque and LinkedList]

--- src/algorithm/binaryTree/a_102_levelOrder/Solution.java
+++ src/algorithm/binaryTree/a_102_levelOrder/Solution.java
@@ -2,10 +2,7 @@ package algorithm.binaryTree.a_102_levelOrder;
 
 import algorithm.binaryTree.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.*;
 
 /**
  * 102. 二叉树的层序遍历
@@ -13,21 +10,21 @@ import java.util.concurrent.LinkedBlockingQueue;
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedBlockingQueue<>();
+        Deque<TreeNode> queue = new LinkedList<>();
         if (root != null) {
-            queue.offer(root);
+            queue.offerLast(root);
             while (!queue.isEmpty()) {
                 int size = queue.size();
                 List<Integer> levelNode =  new ArrayList<>();
                 for (int i = 0; i < size; i++) {
-                    TreeNode treeNode = queue.poll();
+                    TreeNode treeNode = queue.pollFirst();
                     levelNode.add(treeNode.val);
 
                     if (treeNode.left != null) {
-                        queue.offer(treeNode.left);
+                        queue.offerLast(treeNode.left);
                     }
                     if (treeNode.right != null) {
-                        queue.offer(treeNode.right);
+                        queue.offerLast(treeNode.right);
                     }
                 }
                 result.add(levelNode);
