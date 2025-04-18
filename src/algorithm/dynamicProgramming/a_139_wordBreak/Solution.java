11b6e9b313077cec73d59c508521076bdab2ae68
Kevin Fan <rollsbean@gmail.com>
2025-04-18 13:04:28 +0800

    [Leetcode-139][Add][Word break- dp]
    **实现思路**：在上一版的基础上剪枝，i～j 的长度在字典的长度范围内才判断 contains

--- src/algorithm/dynamicProgramming/a_139_wordBreak/Solution.java
+++ src/algorithm/dynamicProgramming/a_139_wordBreak/Solution.java
@@ -11,11 +11,17 @@ import java.util.Set;
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> dictSet = new HashSet<>(wordDict);
+        int maxLength = 1;
+        int minLength = 20;
+        for (String string : dictSet) {
+            maxLength = Math.max(string.length(), maxLength);
+            minLength = Math.min(string.length(), minLength);
+        }
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
-                if (dp[j] && dictSet.contains(s.substring(j, i))) {
+                if (dp[j] && i - j <= maxLength && i - j >= minLength && dictSet.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
